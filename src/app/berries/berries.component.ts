87fbe95deed8746ab7197df5e3e68fbb441e6a11
Allison Schambers <aschambers4291@gmail.com>
2023-12-04 20:33:20 -0800

    add method to sort pokemon by a specified berry

--- src/app/berries/berries.component.ts
+++ src/app/berries/berries.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Icons } from '@pkmn/img';
 import { PokemonService } from '../pokemon.service';
+import { Pokemon } from '../pokemon.model';
 
 @Component({
   selector: 'app-berries',
@@ -11,11 +12,9 @@ export class BerriesComponent implements OnInit {
   constructor(private pokemonService: PokemonService) {}
 
   allBerries: Array<string> = [];
+  belueBerry: Array<Pokemon> = [];
 
   ngOnInit(): void {
-    const pokemonList = this.pokemonService.retrievePokemonList();
-    console.log(pokemonList);
-
     const allBerries = this.pokemonService.retrieveBerryList();
     this.allBerries = allBerries;
     for (let i = 0; i < allBerries.length; i++) {
@@ -32,5 +31,16 @@ export class BerriesComponent implements OnInit {
     const style = Icons.getItem(`${item} berry`).style;
     
     return style;
+  }
+
+  /**
+   * returns a list of pokemon with a specified berry
+   * @param berry string
+   * @returns array
+   */
+  sortPokemonByBerry = (berry: string) => {
+    const pokemonList = this.pokemonService.retrievePokemonList() || [];
+    const pokemonWithBerry = pokemonList.filter(pokemon => pokemon.ingredient.toLowerCase() === berry);
+    return pokemonWithBerry;
   }
 }
