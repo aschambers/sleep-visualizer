b1782021ccbbf7df2ea95ec7f9c61a129c0a1167
aschambers <allisonschambers20@gmail.com>
2024-04-30 10:01:16 -0700

    Make style changes to the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,30 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Icons } from '@pkmn/img';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  ngOnInit(): void {
-    this.getSprite('Grepa Berry', 'berries');
-    this.getSprite('Leek', 'ingredients');
-    this.getSprite('tr08', 'skills');
-    this.getSprite('twistedspoon', 'recipes');
-  }
-
-  /**
-   * retrieves an icon to show in the menu
-   * @param icon string
-   * @returns string
-   */
-  getSprite = (icon: string, element: string) => {
-    const span = document.getElementById(element);
-    const style = Icons.getItem(icon).style;
-    if (span) {
-      span.setAttribute('style', style);
-    }
-  }
-}
+export class HeaderComponent { }
