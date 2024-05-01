c5f4ad41beba9bc6a23cf62f03ea52d7169c0487
aschambers <allisonschambers20@gmail.com>
2024-04-30 21:15:40 -0700

    Adjust default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { SkillsComponent } from './pages/skills/skills.component';
 import { RecipesComponent } from './pages/recipes/recipes.component';
 
 const routes: Routes = [
-  { path: '', component: BerriesComponent },
+  { path: '', component: RecipesComponent },
   { path: 'berries', component: BerriesComponent },
   { path: 'ingredients', component: IngredientsComponent },
   { path: 'skills', component: SkillsComponent },
